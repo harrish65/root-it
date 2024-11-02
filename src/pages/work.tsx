4a1c635dc69069db6774574e32f4800ba0a3bed7
harrish65 <karthikeyan9629558803@gmail.com>
2024-11-02 17:02:13 +0530

    set

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,7 +1,7 @@
 import DiscussCard from "@/components/DiscussCard";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import CaseStudies from "@/components/CaseStudies";
+import CaseStudies from "@/components/caseStudies";
 import React from "react";
 
 const OurWork: React.FC = () => {
